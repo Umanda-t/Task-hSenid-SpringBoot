94445c8cd4d420ff02396151e6d40b5152b3be80
Umanda <umandathathsaranigalagamage@gmail.com>
2023-01-26 13:02:49 +0530

    Add Student Test

--- src/test/java/com/example/assignment/StudentsRepositoryTest.java
+++ src/test/java/com/example/assignment/StudentsRepositoryTest.java
@@ -2,12 +2,15 @@ package com.example.assignment;
 import static org.assertj.core.api.Assertions.assertThat;
 import com.example.assignment.model.Students;
 import com.example.assignment.repository.StudentsRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.annotation.Rollback;
+
+import java.util.Optional;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -29,5 +32,34 @@ public class StudentsRepositoryTest {
         Students exist = entityManager.find(Students.class, s.getId());
 
         assertThat(o.getName()).isEqualTo(exist.getName());
+    }
+
+
+    @Test
+    public void testGetID()
+    {
+        Long id= Long.valueOf(1);
+        Optional<Students> o= repo.findById(id);
+        Assertions.assertThat(o).isPresent();
+
+    }
+    @Test
+    public void testGetNamebyID()
+    {
+        Long id= Long.valueOf(9);
+        String o= repo.findStudentname(id);
+        Assertions.assertThat(o).isEqualTo("K.J. Ashini");
+
+    }
+
+    @Test
+    public void testDelete()
+    {
+        Long id= Long.valueOf(6);
+        repo.deleteById(id);
+        Optional<Students> o= repo.findById(id);
+        Assertions.assertThat(o).isNotPresent();
+
+
     }
 }
